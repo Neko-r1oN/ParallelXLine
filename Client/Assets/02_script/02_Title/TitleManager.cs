4b30f9605164d329d8cf5620d3c6c2a10b2f86e6
S-JOBI23-054\student <231076@st.yoshida.-g.ac.jp>
2025-02-17 17:11:43 +0900

    コード微調整

--- Client/Assets/02_script/02_Title/TitleManager.cs
+++ Client/Assets/02_script/02_Title/TitleManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using DG.Tweening;                   //DOTweenを使うときはこのusingを入れる
+using KanKikuchi.AudioManager;
 
 public class TitleManager : MonoBehaviour
 {
@@ -19,12 +20,31 @@ public class TitleManager : MonoBehaviour
         menu.SetActive(false);              //メニューを非表示
         isMenuFlag = false;                 //メニューフラグを無効化
 
+        //全てのBGMをフェードアウト
+        BGMManager.Instance.FadeOut();
+
         //BGM再生
-        
+        BGMManager.Instance.Play(
+            audioPath: BGMPath.TITLE, //再生したいオーディオのパス
+            volumeRate: 1,                //音量の倍率
+            delay: 0,                //再生されるまでの遅延時間
+            pitch: 1,                //ピッチ
+            isLoop: true,             //ループ再生するか
+            allowsDuplicate: false             //他のBGMと重複して再生させるか
+        );
     }
 
     public void OpenOptionButton()
     {
+        SEManager.Instance.Play(
+            audioPath: SEPath.TAP, //再生したいオーディオのパス
+            volumeRate: 1,                //音量の倍率
+            delay: 0,                //再生されるまでの遅延時間
+            pitch: 1,                //ピッチ
+            isLoop: false,             //ループ再生するか
+            callback: null              //再生終了後の処理
+        );
+
         menu.SetActive(true);
         isMenuFlag = true;
         
@@ -32,6 +52,15 @@ public class TitleManager : MonoBehaviour
 
     public void CloseOptionButton()
     {
+        SEManager.Instance.Play(
+            audioPath: SEPath.TAP, //再生したいオーディオのパス
+            volumeRate: 1,                //音量の倍率
+            delay: 0,                //再生されるまでの遅延時間
+            pitch: 1,                //ピッチ
+            isLoop: false,             //ループ再生するか
+            callback: null              //再生終了後の処理
+        );
+
         isMenuFlag = false;
         Invoke("CloseMenu", 0.5f);
         
@@ -42,9 +71,21 @@ public class TitleManager : MonoBehaviour
         menu.SetActive(false);
     }
 
-    public void StartGame()
+    public void OnClickStart()
     {
+        SEManager.Instance.Play(
+            audioPath: SEPath.TAP, //再生したいオーディオのパス
+            volumeRate: 1,                //音量の倍率
+            delay: 0,                //再生されるまでの遅延時間
+            pitch: 1,                //ピッチ
+            isLoop: false,             //ループ再生するか
+            callback: null              //再生終了後の処理
+        );
 
+        //全てのBGMをフェードアウト
+        BGMManager.Instance.FadeOut(BGMPath.TITLE, 3, () => {
+            Debug.Log("BGMフェードアウト終了");
+        });
     }
 
 }
---
+++ Client/Assets/06_Data/01_SaveData/SaveData.cs
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////////
+///
+///  ユーザーセーブデータスクリプト
+///  Author : 川口京佑  2025.01/28
+///
+////////////////////////////////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionData
+{
+    public string Name { get; set; }
+    public int UserID { get; set; }
+    public string AuthToken { get; set; }
+}
